5f6e27c2182515106ba0ac23728fcc7da67e7732
MinhGioChai <minhnd2005.soc@gmail.com>
2025-11-17 19:24:22 +0700

    Update ALLOWED_HOSTS

--- weatherProject/weatherProject/settings.py
+++ weatherProject/weatherProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0(kwtkj-5%6wx)r4c)&yn*3=$hsu29)i$lao8_o98_m9@mze1k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
